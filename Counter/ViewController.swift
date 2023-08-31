e9e409026faa0dc92050ea8570d72dab1c45f5e9
Vitalii Tungrikov <tungrikov@mail.ru>
2023-08-31 12:42:13 +0300

    В AppDelegate добавлен вывод на консоль для тестирования коммита через систему контроля версий

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func Button(_ sender: Any) {
+        count += 1
+        label.text = "\(count)"
+    }
 }
 
